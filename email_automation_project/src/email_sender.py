33363ee2e2e54fa82dda27551b238c3d34d5d491
Akash_suresh <108061866+a7k4a6s3h0@users.noreply.github.com>
2023-08-15 17:02:54 +0530

    Update email_sender.py

--- email_automation_project/src/email_sender.py
+++ email_automation_project/src/email_sender.py
@@ -7,7 +7,8 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-sys.path.append('D:\pythonProject29\email_automation_project')  # Adjust this path accordingly
+sys.path.append('/path/to/your/project')  # Adjust this path accordingly
+#eg : D:\pythonProject29\email_automation_project
 from config.config import SMTP_SERVER, SMTP_PORT, SENDER_EMAIL, SENDER_PASSWORD
 
 # create email to user
